9e6f9afdfb457c80bea516096d3635fd3575219b
Antônio Guimarães <acgfbr@gmail.com>
2025-02-19 16:41:20 -0300

    avada kedavra eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@next/next/no-html-link-for-pages": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@next/next/no-img-element": "off",
+      "@typescript-eslint/no-empty-object-type": "off"
+    },
+  },
 ];
 
 export default eslintConfig;
